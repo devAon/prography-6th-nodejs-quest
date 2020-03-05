dc26fac4e2734e25beeb48bf9168503e12952820
devAon <yewon9742@gmail.com>
2020-03-05 22:51:47 +0900

    app.js 수정

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+
 app.use('/hello-world', (req, res) => {
   res.send('hello world');
 })
