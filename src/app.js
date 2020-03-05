fcb8f134efdd3a0572648113a3b26125ad6ea111
devAon <yewon9742@gmail.com>
2020-03-06 03:27:09 +0900

    todo api 수정

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
